67a8847172c59a28f5c5d9335a8e6aafbd93a68f
Piotr Chudziak <strangelove44@gmail.com>
2019-04-15 22:45:15 +0100

    add login form template

--- my_app/messageboard/views.py
+++ my_app/messageboard/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from .models import Message
 
 def home(request):
 	all_messages = Message.objects.all
 	return render(request, 'index.html', {'all_messages': all_messages})
+
+
+def login_user(request):
+	return render(request, 'login.html', {})
+
 
 def about(request):
 	return render(request, 'about.html', {})
