f08d20a274f82a76e2cbc69e0c7303f665733259
Piotr Chudziak <strangelove44@gmail.com>
2019-04-15 23:01:04 +0100

    add working login form

--- my_app/messageboard/urls.py
+++ my_app/messageboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
 	path('login/', views.login_user, name="login"),
+	path('main/', views.main, name="main"),
 ]
--- my_app/messageboard/views.py
+++ my_app/messageboard/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .models import Message
 
 def home(request):
-	all_messages = Message.objects.all
-	return render(request, 'index.html', {'all_messages': all_messages})
+	return render(request, 'index.html', {})
 
 
 def login_user(request):
-	return render(request, 'login.html', {})
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('main')
+		else:
+			return redirect('login')
+	else:
+		return render(request, 'login.html', {})
 
 
 def about(request):
 	return render(request, 'about.html', {})
+
+
+def main(request):
+	all_messages = Message.objects.all
+	return render(request, 'main.html', {'all_messages': all_messages})
